a8e0843a59b6a1743bbe1ae403478681222578be
mpetkevic <miroslav.petkevic@gmail.com>
2019-07-20 12:52:25 +0300

    Simple server ready

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const environment = require('custom-env');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+environment.env('local');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req,res) => {
+  res.send('App is working');
+})
+
+app.listen(port, () => console.log(`Server is working on port: ${port}`))
