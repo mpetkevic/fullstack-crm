2632153728caf783161ad4ae57fd4b48e1307767
mpetkevic <miroslav.petkevic@gmail.com>
2019-07-21 14:56:05 +0300

    [React + Node] User login implemented with DB and Encryption

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const User = require('./../dbmodels/user');
 
@@ -14,7 +15,13 @@ router.post('/login', (req, res) => {
   })
     .then(result => {
       if(result.length > 0){
-        res.status(200).send(result[0].dataValues)
+        bcrypt.compare(userInfo.password, result[0].dataValues.password, (error, hashed) => {
+          if(hashed) {
+            res.status(200).send(result[0]);
+          } else {
+            res.status(400).send({error: 'Wrong Credentials. Try again'});
+          }
+        })
       } else {
         res.status(400).send({error: 'Wrong Credentials. Try again'});
       }
@@ -24,14 +31,17 @@ router.post('/login', (req, res) => {
 
 router.post('/register', (req, res) => {
   const userInfo = req.body;
-  User
-    .create({
-      email: userInfo.email,
-      password: userInfo.password,
-      role: userInfo.email === 'admin@admin.com' ? 'ADMIN' : 'USER'
-    })
-    .then(result => res.status(200).send(result))
-    .catch(err => res.status(400).send(err.parent.code));
+  bcrypt.hash(userInfo.password, 10)
+    .then(hashed => {
+      User
+        .create({
+          email: userInfo.email,
+          password: hashed,
+          role: userInfo.email === 'admin@admin.com' ? 'ADMIN' : 'USER'
+        })
+        .then(result => res.status(200).send(result))
+        .catch(err => res.status(400).send(err.parent.code));
+    });
 })
 
 module.exports = router;
